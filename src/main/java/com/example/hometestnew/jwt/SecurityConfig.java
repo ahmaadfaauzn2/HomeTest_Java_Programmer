052f0b48e2940ea3354907c8b6635ea0aaa27e07
Ahmad Fauzan <134770763+ahmaadfaauzn2@users.noreply.github.com>
2024-11-06 16:28:43 +0700

    Update SecurityConfig.java

--- src/main/java/com/example/hometestnew/jwt/SecurityConfig.java
+++ src/main/java/com/example/hometestnew/jwt/SecurityConfig.java
@@ -1,17 +1,13 @@
 package com.example.hometestnew.jwt;
 
 
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -25,16 +21,13 @@ import java.util.logging.Logger;
 
 public class SecurityConfig {
 //    private static final Logger logger = (Logger) LoggerFactory.getLogger(SecurityConfig.class);
-    @Bean
-    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
-        StrictHttpFirewall firewall = new StrictHttpFirewall();
-        firewall.setAllowUrlEncodedPercent(true);  // Allow URL encoded %
-        firewall.setAllowUrlEncodedSlash(true);    // Allow URL encoded /
-        firewall.setAllowBackSlash(true);          // Allow backslash
-        firewall.setAllowSemicolon(true);          // Allow semicolon
-        firewall.setAllowUrlEncodedDoubleSlash(true);  // Allow double slash
-        return firewall;
-    }
+        @Bean
+        public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+            StrictHttpFirewall firewall = new StrictHttpFirewall();
+            firewall.setAllowUrlEncodedPercent(true); // Allows % encoded characters
+            return firewall;
+        }
+
 
     // Bean to configure CORS in Spring Security
     @Bean
@@ -53,12 +46,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .requestMatchers("/login", "/registration", "/profile", "/profile/update", "/profile/image", "/banner", "/services", "/balance", "/topup", "/transaction", "/transaction/history", "/v1/api-docs", "/v2/api-docs", "/v3/api-docs","/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html", "/auth-controller/**", "/error").permitAll()
-                .anyRequest().authenticated()
-
-                .and()
-                .csrf(csrf -> csrf.disable());
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/registration", "/profile", "/profile/update", "/profile/image",
+                                "/banner", "/services", "/balance", "/topup", "/transaction", "/transaction/history",
+                                "/v1/api-docs", "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
+                                "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
+                                "/webjars/**", "/swagger-ui.html", "/auth-controller/**", "/error")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(customCorsConfigurationSource()));
         return http.build();
     }
 
