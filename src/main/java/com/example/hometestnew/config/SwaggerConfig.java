3add6a316b4522bc32754e9fd29427711ad25f50
Ahmad Fauzan <134770763+ahmaadfaauzn2@users.noreply.github.com>
2024-11-06 16:18:33 +0700

    SwaggerConfig.java

--- src/main/java/com/example/hometestnew/config/SwaggerConfig.java
+++ src/main/java/com/example/hometestnew/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +15,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://hometestjavaprogrammer-production.up.railway.app"))
                 .info(new Info().title("Home Test API"))
                 .components(new Components().addSecuritySchemes("JavaInUseSecurityScheme",
                         new SecurityScheme()
                                 .name("JavaInUseSecurityScheme")
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
-                                .bearerFormat("JWT")));
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("JavaInUseSecurityScheme"));
     }
 }
+
